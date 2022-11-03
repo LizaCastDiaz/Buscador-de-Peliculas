a7f1dd49d897ecab70b75d7db8520ee81ed221f6
Liza <Lizacastdiaz@gmail.com>
2022-11-03 22:22:47 +0100

    Creating the folder view and adding router.js

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const db = require('./db/db');
 const router = require('./router'); //llamamos al archivo router
 const PORT = 3000;  //puerto de conexion
 
