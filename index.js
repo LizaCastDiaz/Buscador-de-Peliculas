ee4bf3cc30b5413342f6ac29035a9f5b3865899d
Liza <Lizacastdiaz@gmail.com>
2022-11-03 21:33:23 +0100

    Starting over again. From the beginning

--- index.js
+++ index.js
@@ -1,53 +1,11 @@
-const express = require("express");
+const express = require('express');
 const app = express();
-const PORT = 3000;
 const router = require('./router');
-
-
-//middlewares
-app.use(express.json()); //PUEDO OBTENER JSON DEL BODY
+const PORT = 3000;
+app.use(express.json());
 app.use(router);
 
 
-
-//Servidor en escucha. Conectado a la base de datos
-app.listen(PORT, () => {     
-    console.log(`Servidor conectado en el puerto  ${PORT}`);     
-    db.authenticate().then(()=> {         
-        console.log("Conectado a la base de datos");     
-    }).catch(error => {         
-        console.log('No se ha conectado a la base de datos' + error)     
-    })
-    
+app.listen(PORT, () =>{
+    console.log(`servidor corriendo en el puerto ${PORT}`);
 });
-
-// const morgan = require('morgan');
-// const models = require('./models/index');
-// const sequelize = require('./db/db');
-// const db = require('./db/db');
-
-
-// const bodyParser = require('body-parser');
-// const colors = require('colors');
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
